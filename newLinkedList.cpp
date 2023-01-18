400c4f8ba2790205bcd3687a7f571b9baa54b16b
Gabriela Zuber <102901729+vampiregrin@users.noreply.github.com>
2023-01-18 18:05:48 +0100

    Add files via upload

--- newLinkedList.cpp
+++ newLinkedList.cpp
@@ -1,104 +1,110 @@
 #include <cassert>
+#include <cstddef>
 #include <iostream>
+#include <iterator>
 #include <regex>
+#include <utility>
 #include <vector>
 
-class Node {
+template <typename T> class Node {
 public:
-  std::string value; // word
+  T value;
   Node *next;
   Node *prev;
 };
 
-void printList(Node *n) {
-  while (n != NULL) {
-    std::cout << n->value << std::endl;
-    n = n->next;
-  }
+template <typename T> class List {
+private:
+  Node<T> *head;
+  Node<T> *tail;
+
+public:
+  List();
+  Node<T> *getHead();
+  Node<T> *getTail();
+  T &getNthValue(int index);
+  void addAtEnd(T newVal);
+  void addAfterTail(T newVal);
+  void addAtBeginning(T newVal);
+  int listSize();
+  bool insertAtGivenPosition(int x, T newVal);
+  void printList();
+  void printListBackwards();
+  bool deleteNode(int x);
+};
+
+template <typename T> List<T>::List() {
+  head = NULL;
+  tail = NULL;
 }
 
-void printListBackwards(Node **p_head) {
-  Node *head = *p_head;
-  std::cout << __LINE__ << "  head:" << head << "  head value:" << head->value
-            << "  &head: " << &head << std::endl
-            << std::endl;
-  std::cout << __LINE__ << "  p_head:" << p_head << "  &p_head: " << &p_head
-            << std::endl
-            << std::endl;
-  Node *tail = head;
-  std::cout << __LINE__ << "  tail:" << tail << "  tail value:" << tail->value
-            << "  &tail:" << &tail << std::endl;
-
-  // Traversing till tail of the linked list
-  while (tail->next != NULL) {
-    tail = tail->next;
-    std::cout << "tail value:  " << tail->value << std::endl;
-  }
-
-  // Traversing linked list from tail and printing the node->value
-  while (tail != head) {
-    std::cout << tail->value << " ";
-    tail = tail->prev;
-  }
-  std::cout << tail->value << std::endl;
-}
-
-void insertAtTheFront(Node **head, std::string newValue) {
-  // 1. Prepare a new Node
-  Node *newNode = new Node();
-  newNode->value = newValue;
-  // 2. Put it in front of current head
-  newNode->next = *head;
-  // 3. Move head of the list to point of the newNode
-  *head = newNode;
-}
-
-void insertAtTheEnd(Node **head, std::string newValue)
-// Dodawanie na koncu, ale ta metoda nie wykorzystuje ogona
-{
-  assert(head != NULL);
-
-  // 1. Prepare a new Node
-  Node *newNode = new Node();
-  newNode->value = newValue;
-  newNode->next = NULL;
-  // 2. Check if not empty, if yes -> newNode = head
-  if (*head == NULL || head == 0) {
-    newNode->prev = NULL;
-    *head = newNode;
-    return;
+template <typename T> Node<T> *List<T>::getTail() {
+  if (head == NULL) {
+    std::cout << "NULL" << std::endl;
+    return NULL;
   } else {
-    // 3. Find the last Node
-    Node *last = *head; // we start at the first node & move to the next one
-    while (last->next != NULL) {
-      last = last->next;
+    Node<T> *current = head;
+    if (current->next == NULL) {
+      tail = current;
+    } else {
+      while (current->next != NULL) {
+        current = current->next;
+        tail = current;
+      }
     }
-    // 4. Insert newNode after last node
-    last->next = newNode;
   }
+  std::cout << "tail: " << tail->value;
+  return tail;
 }
 
-// void insertAtTheGivenPosition (Node ** head, std::string newValue) {
-//   if (prev = NULL)
-// }
+template <typename T> Node<T> *List<T>::getHead() { return head; }
 
-std::vector<std::string> splitSentence(std::string sen) {
-  assert(!sen.empty()); // mozna tak?
-  std::vector<std::string> vecOfWords;
-  std::regex rgx("\\!\\s*");
-  std::sregex_token_iterator iter(sen.begin(), sen.end(), rgx, -1);
-  std::sregex_token_iterator end;
-  for (; iter != end; ++iter) {
-    vecOfWords.push_back(*iter);
+template <typename T> void List<T>::addAtEnd(T newVal) {
+
+  Node<T> *newNode = new Node<T>();
+
+  newNode->value = newVal;
+  newNode->next = NULL;
+
+  if (head == NULL) {
+    newNode->prev = NULL;
+    head = newNode;
+  } else {
+    tail = getTail();
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
   }
-  return vecOfWords;
 }
+template <typename T> void List<T>::addAtBeginning(T newVal) {
 
-std::string GetNth(Node *head, int index) {
+  Node<T> *newNode = new Node<T>();
+  newNode->value = newVal;
+  Node<T> *current = head;
+  head = newNode;
+  head->next = current;
+}
+template <typename T> void List<T>::addAfterTail(T newVal) {
 
-  Node *current = head;
+  Node<T> *newNode = new Node<T>();
 
-  // the index of the node we're currently looking at
+  newNode->value = newVal;
+  newNode->next = NULL;
+
+  if (head == NULL) {
+    newNode->prev = NULL;
+    head = newNode;
+  } else {
+    tail = getTail();
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+  }
+}
+template <typename T> T &List<T>::getNthValue(int index) {
+
+  Node<T> *current = head;
+
   int count = 0;
   while (current != NULL) {
     if (count == index)
@@ -106,36 +112,230 @@ std::string GetNth(Node *head, int index) {
     count++;
     current = current->next;
   }
-  return ".";
+  return current->value;
+}
+template <typename T> int List<T>::listSize() {
+  Node<T> *current = head;
+
+  int count = 0;
+  while (current != NULL) {
+    count++;
+    current = current->next;
+  }
+  return count;
+}
+template <typename T> bool List<T>::insertAtGivenPosition(int x, T newVal) {
+
+  int size = listSize();
+  Node<T> *newNode = new Node<T>();
+  newNode->value = newVal;
+  Node<T> *current = head;
+  Node<T> *prevNode;
+
+  if (x == 0) {
+    head = newNode;
+    head->next = current;
+    return true;
+  } else if (x == size + 1) {
+    addAtEnd(newVal);
+    return true;
+  } else if (x > size + 1) {
+    return false;
+  } else {
+    int count = 0;
+    while (current != NULL) {
+
+      if (count == x) {
+        prevNode = current->prev;
+        break;
+      }
+      count++;
+      current = current->next;
+    }
+    prevNode->next = newNode;
+    newNode->next = current;
+    return true;
+  }
+}
+template <typename T> void List<T>::printList() {
+  if (listSize() == 0) {
+    std::cout << "List is empty" << std::endl;
+  } else {
+    Node<T> *current = head;
+    int count = 0;
+    while (current != NULL) {
+      std::cout << count << ".";
+      std::cout << current->value << std::endl;
+      count++;
+      current = current->next;
+    }
+  }
 }
 
-// void test_addElemAtTheEnd() {
-//   List l;
-//   Node* tail_before_extending = l.tail;
-//   Node* n = new Node();
-//   l.addAtEnd(n);
-//   assert(l.tail == n);
-//   assert(n->prev == tail_before_extending);
-//   assert(tail_before_extending ->next == l.tail);
-// }
+template <typename T> void List<T>::printListBackwards() {
+  if (listSize() == 0) {
+    std::cout << "List is empty" << std::endl;
+  } else {
+    tail = getTail();
+    Node<T> *current = tail;
+    int count = listSize() - 1;
+    while (current != head) {
+      std::cout << count << ".";
+      std::cout << current->value << std::endl;
+      count--;
+      current = current->prev;
+    }
+    std::cout << "0." << head->value << std::endl;
+  }
+}
 
+template <typename T> bool List<T>::deleteNode(int x) {
+
+  int size = listSize();
+  Node<T> *current = head;
+  Node<T> *prevNode;
+  Node<T> *nextNode;
+
+  if (x == 0 && size == 1) {
+    head = NULL;
+    return true;
+  } else if (x == 0) {
+    head->next = head;
+    std::cout << head->value << std::endl;
+    return true;
+  } else if (x > size + 1) {
+    return false;
+  } else if (size == 0) {
+    return false;
+  } else if (x == size) {
+    tail->prev = tail;
+    tail->next = NULL;
+    return true;
+  } else {
+    int count = 0;
+    while (current != NULL) {
+      if (count == x) {
+        prevNode = current->prev;
+        nextNode = current->next;
+        prevNode->next = nextNode;
+        break;
+      }
+      count++;
+      current = current->next;
+    }
+    return true;
+  }
+}
+
+List<std::string> createSampleListOfStrings() {
+  List<std::string> l = List<std::string>();
+  l.addAtEnd("first");
+  l.addAtEnd("second");
+  l.addAtEnd("third");
+  l.addAtEnd("fourth");
+  return l;
+}
+
+List<int> createSampleListOfInts() {
+  List<int> l = List<int>();
+  l.addAtEnd(1);
+  l.addAtEnd(2);
+  l.addAtEnd(3);
+  l.addAtEnd(4);
+  return l;
+}
+void test_addAtEndOfEmptyString() {
+  List<std::string> l = List<std::string>();
+
+  l.addAtEnd("Hello");
+  assert(l.getHead()->value == "Hello");
+  assert(l.getTail()->value == "Hello");
+}
+
+void test_addAtEndOfEmptyInt() {
+  List<int> l = List<int>();
+  l.addAtEnd(100);
+  assert(l.getHead()->value == 100);
+  assert(l.getTail()->value == 100);
+}
+
+void test_addAtEndOfNotEmptyInt() {
+  List<std::string> l = createSampleListOfStrings();
+  l.addAtEnd("fifth");
+  assert(l.getHead()->value == "first");
+  assert(l.getTail()->value == "fifth");
+}
+
+void test_emptyListInt() {
+  List<int> l = List<int>();
+  assert(l.getHead() == NULL);
+  assert(l.getTail() == NULL);
+}
+void test_getTailOfNonEmpty() {
+  List<std::string> l = createSampleListOfStrings();
+  assert(l.getTail()->value == "fourth");
+}
+
+void test_getNthValue() {
+  List<std::string> l = createSampleListOfStrings();
+  assert(l.getNthValue(0) == "first");
+  assert(l.getNthValue(2) == "third");
+}
+
+void test_insertAtGivenPosition() {
+  List<int> l = createSampleListOfInts();
+  l.insertAtGivenPosition(0, 0);
+  assert(l.getNthValue(0) == 0);
+  assert(l.getNthValue(1) == 1);
+  l.insertAtGivenPosition(l.listSize() + 1, 5);
+  assert(l.getNthValue(5) == 5);
+  assert(l.insertAtGivenPosition(10, 10) == false);
+}
+void test_listSize() {
+  List<int> l = createSampleListOfInts();
+  assert(l.listSize() == 4);
+}
+void test_addAfterTail() {
+  List<std::string> l = createSampleListOfStrings();
+  l.addAfterTail("last");
+  assert(l.getTail()->value == "last");
+}
+void test_addAtBeginning() {
+  List<std::string> l = createSampleListOfStrings();
+  l.addAtBeginning("zero");
+  assert(l.getHead()->value == "zero");
+}
+
+void test_deleteNodeString() {
+  List<std::string> l = createSampleListOfStrings();
+  l.deleteNode(2);
+  assert(l.getNthValue(2) == "fourth");
+  assert(l.listSize() == 3);
+  assert(l.deleteNode(5) == false);
+}
+
+void test_getHead() {
+  List<int> l = List<int>();
+  assert(l.getHead() == NULL);
+}
 int main() {
 
-  Node *head = nullptr;
-  Node *tail;
+  // test_getTailOfNonEmpty();
+  // test_getNthValue();
+  // test_insertAtGivenPosition();
+  // test_addAfterTail();
+  // test_addAtBeginning();
+  // test_listSize();
+  // test_addAtEndOfEmptyInt();
+  // test_getTailOfNonEmpty();
+  // List<std::string> l = createSampleListOfStrings();
+  //  test_emptyListInt();
+  //   l.printListBackwards();
+  // l.printList();
+  //  l.deleteNode(2);
+  //  test_deleteNodeString();
+  //  l.printList();
+  void test_addAtEndOfNotEmptyInt();
 
-  std::vector<std::string> sentence = splitSentence("Ala! ma! kota!");
-
-  for (int i = 0; i < sentence.size(); i++) {
-    insertAtTheEnd(&head, sentence[i]);
-  }
-  insertAtTheFront(&head, "Uwaga");
-  assert(head->value == "Uwaga");
-
-  // Ciekawe, ze w tym przypadku to zero juz nie zostaje, dlaczego?
-  // printList (head);
-  std::cout << "---------------------" << std::endl;
-  // std::cout << "Element at index .. is: '" << GetNth(head, 1) << "'"
-  // <<std::endl;
-  printListBackwards(&head);
+  return 0;
 }
